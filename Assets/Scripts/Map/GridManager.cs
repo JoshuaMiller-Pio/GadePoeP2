a573d6ddbc1cc062c3ac562f24cd8f126848b3b3
Joshua Miller-Pio <joshuapio585@gmail.com>
2024-04-25 19:19:02 +0200

    Updated procgen

--- Assets/Scripts/Map/GridManager.cs
+++ Assets/Scripts/Map/GridManager.cs
@@ -6,13 +6,13 @@ using UnityEngine.UIElements;
 public class GridManager : MonoBehaviour
 {
     public float length, breath, displacementt,secondDisplace;
-    
+    private float DesertBioms;
     public GameObject forest, snow, lava, mountain, desert;
     // Start is called before the first frame update
     void Start()
     {
 
-        displacementt = Random.Range(5, 10);
+        displacementt = Random.Range(5, 12);
         secondDisplace = Random.Range(8, 30);
 
         for (int x = 0; x < length; x++)
@@ -37,6 +37,7 @@ public class GridManager : MonoBehaviour
     {
       
         
+        
         if (position.y >= 10 && position.y < 14)
         {
             return mountain;
@@ -45,13 +46,32 @@ public class GridManager : MonoBehaviour
         {
             return snow;
         }
-        else if (position.y <= 5)
+        else if (position.y <= 4)
         {
             return lava;
         }
         
+        if (DesertBioms <=Random.Range(50,100))
+        {
+            RaycastHit info;
+            if ( DesertBioms <= Random.Range(0,3) && Random.Range(0, 10) == Random.Range(0, 10))
+            {
+                DesertBioms++;
+                return desert;
+            }
+
+            if ((Physics.Raycast(position,Vector3.back,out info,12)&& info.collider.gameObject.name == "Desert(Clone)")||(Physics.Raycast(position,Vector3.left,out info,12) && info.collider.gameObject.name == "Desert(Clone)")||(Physics.Raycast(position,Vector3.forward,out info,12)&& info.collider.gameObject.name == "Desert(Clone)")||(Physics.Raycast(position, Vector3.right, out info, 12)&& info.collider.gameObject.name == "Desert(Clone)"))
+            {
+
+                    DesertBioms++;
+                    Debug.Log(info.collider.gameObject.name);
+                    return desert;
+
+            }
+     
+        }
         
-        
+        Debug.Log("forest");
         return forest;
     }
 }
